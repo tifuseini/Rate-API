41b36bcc57889ac75ab81c24dab49e074c89e870
tifuseini <tifuseini@st.ug.edu.gh>
2022-12-26 20:22:34 +0000

    CurrencyController class

--- src/main/java/com/messaging/rate/controller/CurrencyController.java
+++ src/main/java/com/messaging/rate/controller/CurrencyController.java
@@ -1,4 +1,9 @@
 package com.messaging.rate.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class CurrencyController {
 }
---
+++ src/main/java/com/messaging/rate/service/CurrencyConversionService.java
@@ -0,0 +1,4 @@
+package com.messaging.rate.service;
+
+public class CurrencyConversionService {
+}
