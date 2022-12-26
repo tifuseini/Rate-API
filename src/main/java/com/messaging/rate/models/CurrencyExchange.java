293cb51eed2b0f9b2bec52acda13ccb3b8519a0b
tifuseini <tifuseini@st.ug.edu.gh>
2022-12-26 20:32:59 +0000

    Update

--- src/main/java/com/messaging/rate/models/CurrencyExchange.java
+++ src/main/java/com/messaging/rate/models/CurrencyExchange.java
@@ -1,4 +1,16 @@
 package com.messaging.rate.models;
 
+import lombok.Data;
+
+@Data
 public class CurrencyExchange {
+
+    private static final String BASE_CODE = "USD";
+
+    private String base;
+
+    private String date;
+
+    private String rates;
+
 }
--- src/main/java/com/messaging/rate/service/CurrencyConversionService.java
+++ src/main/java/com/messaging/rate/service/CurrencyConversionService.java
@@ -1,4 +1,15 @@
 package com.messaging.rate.service;
 
+import com.messaging.rate.models.CurrencyConversion;
+import com.messaging.rate.repository.RateRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class CurrencyConversionService {
+
+    private final RateRepository rateRepository;
+
+    public CurrencyConversion convertFromTo(String base )
 }
